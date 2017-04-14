de9f3075108e8327fd763f55123b81d6cce92a4b
Leonardo Cavallucci <leo.cavallucci@gmail.com>
2017-04-14 22:43:04 +0200

    Add basic templates

--- deadlines/views.py
+++ deadlines/views.py
@@ -6,6 +6,7 @@ from .models import Bill
 class BillListView(ListView):
 
     model = Bill
+    context_object_name = 'bills'
 
     def get_context_data(self, **kwargs):
         context = super(BillListView, self).get_context_data(**kwargs)
