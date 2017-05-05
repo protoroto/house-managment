21534b51f134c0d125f31eebabda17e2b9671552
Leonardo Cavallucci <leo.cavallucci@gmail.com>
2017-05-05 23:40:11 +0200

    Fix typo

--- house_management/settings.py
+++ house_management/settings.py
@@ -139,6 +139,6 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication'
+        'rest_framework.authentication.SessionAuthentication',
     ),
 }
